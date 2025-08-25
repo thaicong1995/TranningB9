3c687358d6487385d842fff92f86a2fdd4850a1e
PC <thaicong1995@gmail.com>
2025-08-25 10:16:46 +0700

    First commit

---
+++ src/main/java/org/example/JavaCore/Array.java
@@ -0,0 +1,12 @@
+package org.example.JavaCore;
+
+public class Array {
+    public int[] array( int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            newArr[i] = arr[i];
+        }
+        return newArr;
+
+    }
+}
